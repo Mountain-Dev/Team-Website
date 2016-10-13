2bdd73616afa4be439f30de01066b28cd760f854
Cossack-HD <kazakhd@gmail.com>
2016-10-13 14:19:32 +0200

    Added 4 views and pages (one per person in group)

---
+++ Website/Website/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Website.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Steven()
+        {
+            return View();
+        }
+
+        public ActionResult Felix()
+        {
+            return View();
+        }
+
+        public ActionResult Alex()
+        {
+            return View();
+        }
+
+        public ActionResult Aleksei()
+        {
+            return View();
+        }
+    }
+}
