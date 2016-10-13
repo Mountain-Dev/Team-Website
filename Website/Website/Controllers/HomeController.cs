d6f0cd6aac43fdc69c902c9655ba9d2f33e44bb7
Cossack-HD <kazakhd@gmail.com>
2016-10-13 16:34:21 +0200

    Added fake list of objects to display in the fancy accordion in about view.

--- Website/Website/Controllers/HomeController.cs
+++ Website/Website/Controllers/HomeController.cs
@@ -3,11 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Website.Models;
 
 namespace Website.Controllers
 {
+
     public class HomeController : Controller
     {
+        private List<personData> personalDatas;
+
+        public HomeController()
+        {
+            personalDatas = new List<personData>();
+            personalDatas.Add(new personData() { title = "Felix", text = "Test Text Felix" });
+            personalDatas.Add(new personData() { title = "Steven", text = "Test Text Steven" });
+            personalDatas.Add(new personData() { title = "Alex", text = "Test Alex" });
+            personalDatas.Add(new personData() { title = "Aleksei", text = "Test Text Aleksei" });
+        }
+
+
+
         // GET: Home
         public ActionResult Index()
         {
@@ -16,7 +31,7 @@ namespace Website.Controllers
 
         public ActionResult About()
         {
-            return View();
+            return View(personalDatas);
         }
 
         public ActionResult Contact()
