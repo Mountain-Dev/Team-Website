3b9870546477e2227e1ea4d609baa6950579af33
Steven Bergqvist <stevenbergqvist@gmail.com>
2016-10-14 11:11:56 +0200

    Added a info about steven.

--- Website/Website/Controllers/HomeController.cs
+++ Website/Website/Controllers/HomeController.cs
@@ -16,7 +16,7 @@ namespace Website.Controllers
         {
             personalDatas = new List<personData>();
             personalDatas.Add(new personData() { title = "Felix Wetell", phone = 30100000, mail = "felix.wetell@gmail.com", text = "I´m just a random guy that writes shit code!" });
-            personalDatas.Add(new personData() { title = "Steven Bergqvist", phone = 010101, mail = "stevenbergqvist@gmail.com", text = "Steven is a good guy." });
+            personalDatas.Add(new personData() { title = "Steven Bergqvist", phone = 0738518323, mail = "stevenbergqvist@gmail.com", text = "I'm 19 years old and studying programming. I am interested in all things tech and I hope to find a career in this field." });
             personalDatas.Add(new personData() { title = "Alex Söderberg", phone = 987654321, mail = "h1h2@hotmail.com", text = "Alex is a cooool guy." });
             personalDatas.Add(new personData() { title = "Aleksei Kazakov", phone = 123456789, mail = "cossack-hd@gmail.com", text = "Aleksei is a Russian elite hacker." });
         }
